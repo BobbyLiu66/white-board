a975966dbd8baf33714266dce20681286c7d3128
BobbyLiu66 <bobbyliu45@gmail.com>
2018-05-05 15:58:12 +1200

    add static

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static('public'));
 app.use((req, res, next) =>{
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
