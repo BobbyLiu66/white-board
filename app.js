aef40dc739a409f88ab1483582da4e72acf1876d
BobbyLiu66 <bobbyliu45@gmail.com>
2018-05-05 12:50:02 +1200

    enable CORS

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(function(req, res, next) {
+app.use((req, res, next) =>{
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
