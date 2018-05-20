73495e0afb18843158681528195febe68106b8fe
BobbyLiu66 <bobbyliu45@gmail.com>
2018-05-20 21:54:35 +1200

    fix tiny bug about api function

--- src/api.js
+++ src/api.js
@@ -4,7 +4,7 @@ exports.getIpInfo = (clientIp)=>{
     return new Promise((resolve, reject)=>{
         request({
             method: 'GET',
-            url: `http://fp.ip-api.com/json/${clientIp}`,
+            url: `http://ip-api.com/json/${clientIp}`,
         }, function (error, response, body) {
             if (error) reject(error);
             resolve(body);
