92354e386e68e03355be380ad76c4506d732c066
BobbyLiu66 <bobbyliu45@gmail.com>
2018-06-22 14:57:46 +1200

    compress image size

--- src/s3.js
+++ src/s3.js
@@ -1,15 +1,18 @@
 const AWS = require('aws-sdk');
 const bucketName = 'chat-picture';
+const sharp = require('sharp');
 const s3 = new AWS.S3({
     apiVersion: '2006-03-01',
     params: {Bucket: bucketName}
 });
 
 exports.uploadPhoto = async (data) => {
+    const image = await sharp(new Buffer(data.photo.replace(/^data:image\/\w+;base64,/, ""), 'base64')).resize(35, 35).toBuffer();
+
     const uploadParams = {
         Bucket: bucketName,
         Key: `${data.nickname}.png`,
-        Body: new Buffer(data.photo.replace(/^data:image\/\w+;base64,/, ""), 'base64'),
+        Body: image,
         ACL: 'public-read',
         ContentEncoding: 'base64',
         ContentType: 'image/jpeg'
